276d6f8a73906f2e368e073d1fa43d05a04feab2
ronnin796 <nischal.chaudhary796@gmail.com>
2025-11-10 20:48:08 +0545

    Img fixed

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
-from PIL import Image
 
 model = load_model('cat_dog_classifier.keras')
 
@@ -11,16 +10,14 @@ st.title("🐱🐶 Cat vs Dog Classifier")
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Display the original image (high quality)
     original_img = Image.open(uploaded_file)
     st.image(original_img, caption="Uploaded Image", use_container_width=True)
+    img = image.load_img(uploaded_file, target_size=(128, 128))
+    
 
-    # Resize a copy for model input
-    img_resized = original_img.resize((128, 128))
-    x = image.img_to_array(img_resized)
+    x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0) / 255.0
-
-    # Predict
     prediction = model.predict(x)
     label = "Dog 🐶" if prediction[0][0] > 0.5 else "Cat 🐱"
     st.subheader(f"Prediction: {label}")
+
